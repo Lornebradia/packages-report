8e97bdef86d0a2a59f4b958a37fce9e3544d543b
Lorenzo Braschi <lorenzo.braschi@roche.com>
2018-02-01 11:57:16 -0800

    Produced fancy barchart

--- R/03_barchart-packages-built.R
+++ R/03_barchart-packages-built.R
@@ -2,9 +2,16 @@
 
 ## read that csv into a data frame, then ...
 
-## if you use ggplot2, code like this will work:
-ggplot(apt_freqtable, aes(x = Built, y = n)) +
-  geom_bar(stat = "identity")
+read_csv("data/add-on-packages-freqtable.csv")  %>%
+  ## if you use ggplot2, code like this will work:
+  ggplot(aes(x = Built, y = n)) +
+  geom_bar(stat = "identity", aes(fill = Built), show.legend = F) +
+  coord_flip() +
+  ggtitle("Frequency of builds per package",
+          subtitle = "Lorenzo's packages")+
+  theme_minimal()+
+  theme(plot.title = element_text(size = 14, face = "bold")) +
+  ggsave(filename = "figs/built-barchart.png")
 
 ## write this barchart to figs/built-barchart.png
 ## if you use ggplot2, ggsave() will help
